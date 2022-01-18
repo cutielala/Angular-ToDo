3b4a8039722fda02832db2dc8758a75377d76e13
Viola Lin <shuguann@gmail.com>
2022-01-17 22:56:56 -0800

    End of section 3

--- DatingApp/API/Startup.cs
+++ DatingApp/API/Startup.cs
@@ -36,10 +36,13 @@ namespace API
 
            });
             services.AddControllers();
+            services.AddCors();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,6 +58,11 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            
+            app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
+
+          //  app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://1ocalhost:4200"));
 
             app.UseAuthorization();
 
