9801488efd1c25611bde30b2e7d89a8f40626006
Viola Lin <shuguann@gmail.com>
2022-02-03 00:05:03 -0800

    End of section 7

--- DatingApp/API/Startup.cs
+++ DatingApp/API/Startup.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using API.Extentions;
 using API.Interfaces;
+using API.Middleware;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
@@ -49,21 +50,22 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
-            }
+            // if (env.IsDevelopment())
+            // {
+            //     app.UseDeveloperExceptionPage();
+            //     app.UseSwagger();
+            //     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
+            // }
+            app.UseMiddleware<ExceptionMiddleware>();//77.exception Middleware
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
             
-            app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+            app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 
-          //  app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://1ocalhost:4200"));
+           // app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://1ocalhost:4200"));
 
             app.UseAuthorization();
             app.UseAuthentication();
